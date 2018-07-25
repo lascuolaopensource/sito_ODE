ea0f9b41e10188c5d576f127cae29b63aa3423aa
eleonora cappuccio <eleonoracappuccio@MacBook-Pro-di-eleonora.local>
2018-07-25 11:01:27 +0200

    alberatura dei blue print

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import $ from 'jquery';
+
+$('.titolo').css('color','#639bd2',);
+$('.titolo').css('font-size','60px');
+$('.titolo').css('font-family','helvetica');
+$('.paragrafo').css('font-family','helvetica');
+
+
+
 console.log('It\'s working!')
+
