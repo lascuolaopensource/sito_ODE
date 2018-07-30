9763df6467dc1324534c074afd015e5de0d4a809
alessandro <alessandro@ff3300.com>
2018-07-30 09:46:41 +0200

    finitura mattutina

--- www/site/snippets/header.php
+++ www/site/snippets/header.php
@@ -4,7 +4,72 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <script type="application/ld+json">
+  {
+     "@type" : "Organization",
+      "legalName" : "<?php echo $site->title()->text() ?>",
+      "alternateName" : "<?php echo $site->title()->text() ?>",
+      "url" : "<?php echo $site->url() ?>",
+      "description" : "<?php echo $site->description()->text() ?>",
+       "@type" : "ContactPoint", 
+        "streetAddress" : "Via Crispi 5",
+        "addressLocality" : "Bari",
+        "addressRegion" : "Puglia",
+        "addressCountry" : "Italy",
+        "PostalCode" : "70123",
+        "telephone" : "0809148590",
+        "contactType" : "administration",
+        "logo" : "<?= $site->url() ?>/assets/images/logoode.svg" 
+  }
+  </script>
+
+  <?php if ($page->parent()->title() == "Eventi"): ?>
+      <meta name="description" content="<?= excerpt($page->testo()->text(), 200) ?>">
+  <?php else: ?>
+      <meta name="description" content="<?= $site->description()->text() ?>">
+  <?php endif ?>
+
+
+  <?php if (!$page->title() == "Home"): ?>
+      <meta property="og:title"  content="<?php echo $page->title() ?>" />
+      <meta property="twitter:title"  content="<?php echo $page->title() ?>" />
+      <meta property="og:url"  content="<?php echo $page->url() ?>" />
+      <meta property="twitter:url"  content="<?php echo $page->url() ?>" />
+      <?php if($page->parent()->title() == "Eventi"): ?>
+      <meta property="og:description"  content="<?php echo $page->testo() ?>" />
+      <meta property="twitter:description"  content="<?php echo $page->description() ?>" />
+      <meta property="og:url"  content="<?php echo $page->url() ?>" />
+      <meta property="twitter:url"  content="<?php echo $page->url() ?>" />
+      <?php else: ?>
+      <meta property="og:description"  content="<?php echo $page->description() ?>" />
+      <meta property="twitter:description"  content="<?php echo $page->description() ?>" />
+      <meta property="og:url"  content="<?php echo $page->url() ?>" />
+      <meta property="twitter:url"  content="<?php echo $page->url() ?>" />
+      <?php endif; ?>
+  <?php else: ?>
+      <meta property="og:title"  content="<?php echo $site->title() ?>" />
+      <meta property="twitter:title"  content="<?php echo $site->title() ?>" />
+      <meta property="og:description"  content="<?php echo $site->description() ?>" />
+      <meta property="twitter:description"  content="<?php echo $site->description() ?>" />
+
+      <meta property="og:url"  content="<?php echo $site->url() ?>" />
+      <meta property="twitter:url"  content="<?php echo $site->url() ?>" />
+  
+  <?php endif; ?>
+
+
+  <?php if (!$page->images()->isEmpty()): ?>
+
+  <?php foreach($page->images() as $image): ?>
+      <meta property="og:image" content="<?php echo $image->url() ?>" />
+  <?php endforeach ?>
+
+  <?php endif; ?>
+
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+
+
+  
   <style>
   @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
   </style> 
